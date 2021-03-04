a6a8638c6d1c396fbc54d591d92ff768186757c9
Derek Tissaw <45717136+tissawd@users.noreply.github.com>
2021-03-04 11:44:09 -0600

    Completed checkpoint requirements

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+
+let getPokemon = new Promise((resolve, reject) => {
+    fs.readFile('pokemonList.txt', 'utf8', (err, data) => {
+        if (err){
+            return reject(err)
+        }
+        return resolve(data)
+    })
+})
+
+getPokemon.then(pokemon => {
+    let pokeArray = pokemon.split('\n');
+    let results = pokeArray.map(pokemon => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        .then(body => body.json())
+        .then(json => {
+            let typesObj = json.types;
+            let typesArray = typesObj.map(types => types.type.name)
+
+            console.log(`${json.name}: ${typesArray}`)
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
